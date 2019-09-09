9cb0b622611943c0360f5ef0672bee8d7fe97b6a
stephen.caffrey@gmail.com <stephen.caffrey@gmail.com>
2019-09-09 23:07:54 +0100

    small changes

--- src/main/java/com/leaseplanis/data/entities/Event.java
+++ src/main/java/com/leaseplanis/data/entities/Event.java
@@ -1,6 +1,5 @@
 package com.leaseplanis.data.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.leaseplanis.data.enums.Locations;
 import com.leaseplanis.data.enums.Peril;
 import lombok.Builder;
@@ -11,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 @Builder
 public class Event {
-    @JsonIgnore
     private int id;
     private Peril peril;
     private Locations state;
